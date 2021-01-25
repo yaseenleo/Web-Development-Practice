5cdfb76ff6865ed35e38935502846baa7f65583c
Mohammad Yaseen <yaseenleo>
2021-01-25 12:20:59 +0500

    material full screen search panel added and simple search bar

---
+++ HTML & CSS/Search Bar/Material Full Search Transition/js/app.js
@@ -0,0 +1,8 @@
+$('.control').click( function(){
+    $('body').addClass('search-active');
+    $('.input-search').focus();
+  });
+  
+  $('.icon-close').click( function(){
+    $('body').removeClass('search-active');
+  });
